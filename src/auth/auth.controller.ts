ddfd8d47c6501867a4cba4847e12b7b99eaddd25
Kagan Cubukcu <kagancancubukcu@gmail.com>
2023-09-18 13:10:26 +0300

    Added login functionality

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from 'src/user/dto/dto/user.dto';
 import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,4 +11,6 @@ export class AuthController {
   async registerUser(@Body() dto: CreateUserDto) {
     return await this.userService.create(dto);
   }
+  @Post('login')
+  async loginUser(@Body() dto: LoginDto) {}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -25,4 +25,11 @@ export class UserService {
     const { password, ...result } = newUser;
     return result;
   }
+  async findByEmail(email: string) {
+    return await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+  }
 }
