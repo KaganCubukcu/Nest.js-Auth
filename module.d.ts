f5a83e3e43411653e8afe64e9978ebb957374622
Kagan Cubukcu <kagancancubukcu@gmail.com>
2023-09-22 12:00:49 +0300

    Renamed jwtRefreshToken to jwtRefreshTokenKey

--- module.d.ts
+++ module.d.ts
@@ -2,6 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     DATABASE_URL: string;
     jwtSecretKey: string;
-    jwtRefreshToken: string;
+    jwtRefreshTokenKey: string;
   }
 }
