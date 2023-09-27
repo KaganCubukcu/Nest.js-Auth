722ca2d8f65f62cd4eb5cc644855fa457dd272fc
Kagan Cubukcu <kagancancubukcu@gmail.com>
2023-09-27 10:24:01 +0300

    Implemented refresh guards method in auth module

--- src/auth/guards/refresh.guard.ts
+++ src/auth/guards/refresh.guard.ts
@@ -8,7 +8,7 @@ import { Request } from 'express';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class JwtGuard implements CanActivate {
+export class RefreshJwtGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -18,7 +18,7 @@ export class JwtGuard implements CanActivate {
     }
     try {
       const payload = await this.jwtService.verifyAsync(token, {
-        secret: process.env.jwtSecretKey,
+        secret: process.env.jwtRefreshTokenKey,
       });
       request['user'] = payload;
     } catch {
@@ -28,6 +28,6 @@ export class JwtGuard implements CanActivate {
   }
   private extractTokenFromHeader(request: Request) {
     const [type, token] = request.headers.authorization.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
+    return type === 'Refresh' ? token : undefined;
   }
 }
